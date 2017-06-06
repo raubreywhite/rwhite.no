ead77a0c92710174f0f0d867f4aa500ed160bf0c
Richard White <a@rwhite.no>
2017-06-06 16:03:10 +0200

    Update the website

--- _deploy.sh
+++ _deploy.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 
-set -e
-
-[ -z "${GITHUB_PAT}" ] && exit 0
-[ "${TRAVIS_BRANCH}" != "master" ] && exit 0
-
 git config --global user.email "a@rwhite.no"
 git config --global user.name "Richard White"
+
+cd ~/git/rwhite.no/
 
 git add .
 git commit -m"Update the website" || true
 git checkout gh-pages
-git clone -b gh-pages https://raubreywhite@github.com/${TRAVIS_REPO_SLUG}.git book-output
-cd book-output
-cp -r ../_book/* ./
-git add --all *
-git commit -m"Update the book" || true
+git reset --hard master
+
+cd build
+rm -rf public
+rm -rf src
+cp -r * ../
+git add .
+git commit -m"Update the website" || true
 git push -q origin gh-pages
